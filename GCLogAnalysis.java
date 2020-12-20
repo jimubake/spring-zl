12ff703967c50393488329cd509109e8aea524ef
吉姆巴克 <30330969+jimubake@users.noreply.github.com>
2020-12-21 00:03:42 +0800

    Add files via upload

---
+++ GCLogAnalysis.java
@@ -0,0 +1,57 @@
+package gcLogAnalysis;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+
+public class GCLogAnalysis {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        long startMillis = System.currentTimeMillis();
+        // 运行 1 秒
+        long timeoutMills = TimeUnit.SECONDS.toMillis(1);
+        long endMillis = startMillis + timeoutMills;
+        LongAdder counter = new LongAdder();
+        System.out.println("正在执行...");
+        // 缓存一部分对象，进入老年代
+        int cacheSize = 2000;
+        Object[] cacheGarbage = new Object[cacheSize];
+        while (System.currentTimeMillis() < endMillis) {
+            // 生成垃圾对象
+            Object garbage = generateGarbage(100 * 1024);
+            counter.increment();
+            int randomIndex = random.nextInt(2 * cacheSize);
+            if(randomIndex < cacheSize)
+                cacheGarbage[randomIndex] = garbage;
+        }
+        System.out.println("执行结束！共生成对象次数: " + counter.longValue());
+    }
+
+    private static Object generateGarbage(int max) {
+        int randomSize = random.nextInt(max);
+        int type = randomSize % 4;
+        Object result;
+        switch (type) {
+            case 0:
+                result= new int[randomSize];
+                break;
+            case 1:
+                result = new byte[randomSize];
+                break;
+            case 2:
+                result = new double[randomSize];
+                break;
+            default:
+                StringBuilder sb = new StringBuilder();
+                String randomString = "randomString-Anything";
+                while (sb.length() < randomSize){
+                    sb.append(randomString);
+                    sb.append(max);
+                    sb.append(randomSize);
+                }
+                result = sb.toString();
+        }
+        return result;
+    }
+}
